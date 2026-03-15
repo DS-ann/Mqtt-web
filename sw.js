e8afe24f7ffeddb38cae1441e48e0012f1f66493
DS-ann <dsannigrahi5956@gmail.com>
2026-03-15 18:56:07 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,16 +3,35 @@ const urlsToCache = [
   './',
   './index.html',
   './manifest.json',
+  './sw.js',
+  'https://unpkg.com/mqtt/dist/mqtt.min.js',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  'https://twemoji.maxcdn.com/v/latest/twemoji.min.js',
   './icons/icon-192.png',
   './icons/icon-512.png'
 ];
 
+// Install SW and cache files
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
   );
+  self.skipWaiting();
 });
 
+// Activate SW and cleanup old caches
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.map(key => {
+        if(key !== CACHE_NAME) return caches.delete(key);
+      }))
+    )
+  );
+  self.clients.claim();
+});
+
+// Fetch cached resources
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request).then(response => response || fetch(event.request))
